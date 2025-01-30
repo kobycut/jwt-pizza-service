9de367f714c81db291de04dfa35a28432d2bac7d
kobycut <kobycut@gmail.com>
2025-01-29 19:42:46 -0700

    passed

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
 const app = require('../service');
 
+
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
-
 
 beforeAll(async () => {
     testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
@@ -22,6 +22,34 @@ test('login', async () => {
     expect(loginRes.body.user).toMatchObject(expectedUser);
 });
 
+test('register', async () => {
+    const randName = randomName();
+    const registerRes = await request(app).post('/api/auth').send({ name: randName, email: `${randName}@email.com`, password: `${randName}password` });
+    expect(registerRes.status).toBe(200);
+})
+
+test('delete', async () => {
+    const deleteRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(deleteRes.status).toBe(200);
+})
+
+// test('update', async () => {
+//     const adminUser = await createAdminUser();
+//     const loginRes = await request(app).put('/api/auth').send(adminUser);
+//     const authToken = await loginRes.body.token;
+//     const userId = await loginRes.body.user.id;
+
+//     const updateRes = await request(app).put(`/api/auth/:${userId}`).set('Authentication', `Bearer ${authToken}`).send({ email: "a@jwt.com", password: "admin" })
+//     expect(updateRes.status).toBe(200);
+// })
+
 function expectValidJwt(potentialJwt) {
     expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 }
+
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+
