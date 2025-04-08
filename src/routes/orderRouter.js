638ca5bab45af48881c0d460de30a7b847fff43d
kobycut <kobycut@gmail.com>
2025-04-08 15:12:19 -0600

    added chaos testing

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -11,6 +11,26 @@ const Logger = require("../logger.js");
 
 const logger = new Logger(config);
 
+let enableChaos = false;
+orderRouter.put(
+  "/chaos/:state",
+  authRouter.authenticateToken,
+  asyncHandler(async (req, res) => {
+    if (req.user.isRole(Role.Admin)) {
+      enableChaos = req.params.state === "true";
+    }
+
+    res.json({ chaos: enableChaos });
+  })
+);
+
+orderRouter.post("/", (req, res, next) => {
+  if (enableChaos && Math.random() < 0.5) {
+    throw new StatusCodeError("Chaos monkey", 500);
+  }
+  next();
+});
+
 orderRouter.endpoints = [
   {
     method: "GET",
